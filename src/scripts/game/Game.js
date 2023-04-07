b5233905ebd7c5ac3776cb339ac4424df029e921
albinxhafa115 <ax51756@ubt-uni.net>
2023-04-07 23:02:48 +0200

    Added createHero on  game.js

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,6 +1,7 @@
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
 import { Background } from "./Background";
+import { Hero } from "./Hero";
 import { Platform } from "./Platform";
 
 export class Game extends Scene {
@@ -15,11 +16,16 @@ export class Game extends Scene {
             cols: 6,
             x: 200
         });
+        this.createHero();
     }
 
     createPlatform(data) {
         const platform = new Platform(data.rows, data.cols, data.x);
         this.container.addChild(platform.container);
+    }
+    createHero() {
+        this.hero = new Hero();
+        this.container.addChild(this.hero.sprite);
     }
 
     update(dt) {
