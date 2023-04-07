402662ed911a14aa12b6a4d214e0acacf26ee8dc
albinxhafa115 <ax51756@ubt-uni.net>
2023-04-07 22:53:05 +0200

    adding container in platform.js

--- src/scripts/game/Platform.js
+++ src/scripts/game/Platform.js
@@ -5,5 +5,15 @@ export class Platform {
     constructor(rows, cols, x) {
         this.rows = rows;
         this.cols = cols;
+        this.tileSize = PIXI.Texture.from("tile").width;
+        this.width = this.tileSize * this.cols;
+        this.height = this.tileSize * this.rows;
+        this.createContainer(x);
+    }
+
+    createContainer(x) {
+        this.container = new PIXI.Container();
+        this.container.x = x;
+        this.container.y = window.innerHeight - this.height;
     }
 }
