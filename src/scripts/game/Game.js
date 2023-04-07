0f877c8254c7a438892426a845fc928daa9435f8
albinxhafa115 <ax51756@ubt-uni.net>
2023-04-07 23:12:53 +0200

    updated game.js with some changes in created methods

--- src/scripts/game/Game.js
+++ src/scripts/game/Game.js
@@ -1,28 +1,26 @@
 import { App } from "../system/App";
 import { Scene } from "../system/Scene";
 import { Background } from "./Background";
-import { Hero } from "./Hero";
-import { Platform } from "./Platform";
+import { Platforms } from "./Platforms";
 
 export class Game extends Scene {
     create() {
         this.createBackground();
+        this.createHero();
+        this.createPlatforms();
     }
+    createPlatforms() {
+        this.platfroms = new Platforms();
+        this.container.addChild(this.platfroms.container);
+    }
+
+
     createBackground() {
         this.bg = new Background();
         this.container.addChild(this.bg.container);
-        this.createPlatform({
-            rows: 4,
-            cols: 6,
-            x: 200
-        });
-        this.createHero();
     }
 
-    createPlatform(data) {
-        const platform = new Platform(data.rows, data.cols, data.x);
-        this.container.addChild(platform.container);
-    }
+
     createHero() {
         this.hero = new Hero();
         this.container.addChild(this.hero.sprite);
