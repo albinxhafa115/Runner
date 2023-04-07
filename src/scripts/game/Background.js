2f819f5e4bdf7a4ebd2de9e326a63a8c60cf645d
albinxhafa115 <ax51756@ubt-uni.net>
2023-04-07 22:29:02 +0200

    updated background in background.js

---
+++ src/scripts/game/Background.js
@@ -0,0 +1,40 @@
+import * as PIXI from "pixi.js";
+import { App } from "../system/App";
+
+export class Background {
+    constructor() {
+        this.speed = App.config.bgSpeed;
+        this.container = new PIXI.Container();
+        this.createSprites();
+    }
+    createSprites() {
+        this.sprites = [];
+
+        for (let i = 0; i < 3; i++) {
+            this.createSprite(i);
+        }
+    }
+
+    createSprite(i) {
+        const sprite = App.sprite("bg");
+
+        sprite.x = sprite.width * i;
+        sprite.y = 0;
+        this.container.addChild(sprite);
+        this.sprites.push(sprite);
+    }
+    move(sprite, offset) {
+        const spriteRightX = sprite.x + sprite.width;
+
+        const screenLeftX = 0;
+
+        if (spriteRightX <= screenLeftX) {
+            sprite.x += sprite.width * this.sprites.length;
+        }
+
+        sprite.x -= offset;
+    }
+    update() {
+
+    }
+}
