cb904d5e0c31721a43184c93080e56a3de2a5a1a
albinxhafa115 <ax51756@ubt-uni.net>
2023-04-07 23:33:48 +0200

    updated platforms.js and platform.js

--- src/scripts/game/Platform.js
+++ src/scripts/game/Platform.js
@@ -1,28 +1,54 @@
+import * as Matter from 'matter-js';
 import * as PIXI from "pixi.js";
 import { App } from '../system/App';
+// [10]
+import { Diamond } from './Diamond';
+// [/10]
 
 export class Platform {
     constructor(rows, cols, x) {
+        // [10]
+        this.diamonds = [];
+        // [/10]
+
         this.rows = rows;
         this.cols = cols;
+
         this.tileSize = PIXI.Texture.from("tile").width;
         this.width = this.tileSize * this.cols;
         this.height = this.tileSize * this.rows;
+
         this.createContainer(x);
         this.createTiles();
+
+        this.dx = App.config.platforms.moveSpeed;
+        this.createBody();
+        this.createDiamonds();
     }
 
-    get randomData() {
-        this.ranges = App.config.platforms.ranges;
-        let data = { rows: 0, cols: 0, x: 0 };
+    // [10]
+    createDiamonds() {
+        const y = App.config.diamonds.offset.min + Math.random() * (App.config.diamonds.offset.max - App.config.diamonds.offset.min);
 
-        const offset = this.ranges.offset.min + Math.round(Math.random() * (this.ranges.offset.max - this.ranges.offset.min));
+        for (let i = 0; i < this.cols; i++) {
+            if (Math.random() < App.config.diamonds.chance) {
+                this.createDiamond(this.tileSize * i, -y);
+            }
+        }
+    }
 
-        data.x = this.current.container.x + this.current.container.width + offset;
-        data.cols = this.ranges.cols.min + Math.round(Math.random() * (this.ranges.cols.max - this.ranges.cols.min));
-        data.rows = this.ranges.rows.min + Math.round(Math.random() * (this.ranges.rows.max - this.ranges.rows.min));
+    createDiamond(x, y) {
+            const diamond = new Diamond(x, y);
+            this.container.addChild(diamond.sprite);
+            diamond.createBody();
+            this.diamonds.push(diamond);
+    }
+    // [/10]
 
-        return data;
+    createBody() {
+        this.body = Matter.Bodies.rectangle(this.width / 2 + this.container.x, this.height / 2 + this.container.y, this.width, this.height, {friction: 0, isStatic: true});
+        Matter.World.add(App.physics.world, this.body);
+        this.body.gamePlatform = this;
     }
 
     createContainer(x) {
@@ -40,10 +66,26 @@ export class Platform {
     }
 
     createTile(row, col) {
-        const texture = row === 0 ? "platform" : "tile"
+        const texture = row === 0 ? "platform" : "tile" 
         const tile = App.sprite(texture);
         this.container.addChild(tile);
         tile.x = col * tile.width;
         tile.y = row * tile.height;
+    }
+
+
+    // 06
+    move() {
+        if (this.body) {
+            Matter.Body.setPosition(this.body, {x: this.body.position.x + this.dx, y: this.body.position.y});
+            this.container.x = this.body.position.x - this.width / 2;
+            this.container.y = this.body.position.y - this.height / 2;
+        }
+    }
+
+    destroy() {
+        Matter.World.remove(App.physics.world, this.body);
+        this.diamonds.forEach(diamond => diamond.destroy());
+        this.container.destroy();
     }
 }
