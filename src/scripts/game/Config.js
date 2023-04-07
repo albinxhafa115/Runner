80eab7f87337b0ba4dbfbf6ccebf72e0d9b3a53c
Diar Selmani <selmanidiar2@gmail.com>
2023-04-07 22:19:44 +0200

    addedd Platform

--- src/scripts/game/Config.js
+++ src/scripts/game/Config.js
@@ -4,7 +4,22 @@ import { Tools } from "../system/Tools";
 export const Config = {
 
     bgSpeed: 2,
-    hero: {
+    platforms: {
+        ranges: {
+            rows: {
+                min: 2,
+                max: 6
+            },
+            cols: {
+                min: 3,
+                max: 9
+            },
+            offset: {
+                min: 60,
+                max: 200
+            }
+        }
+    }, hero: {
         position: {
             x: 350,
             y: 325
