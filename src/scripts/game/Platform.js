67be9e33c4f1fec0cf15f0b5b874dc7aa82ac132
Diar Selmani <selmanidiar2@gmail.com>
2023-04-07 22:28:56 +0200

    updated platform

--- src/scripts/game/Platform.js
+++ src/scripts/game/Platform.js
@@ -12,6 +12,19 @@ export class Platform {
         this.createTiles();
     }
 
+    get randomData() {
+        this.ranges = App.config.platforms.ranges;
+        let data = { rows: 0, cols: 0, x: 0 };
+
+        const offset = this.ranges.offset.min + Math.round(Math.random() * (this.ranges.offset.max - this.ranges.offset.min));
+
+        data.x = this.current.container.x + this.current.container.width + offset;
+        data.cols = this.ranges.cols.min + Math.round(Math.random() * (this.ranges.cols.max - this.ranges.cols.min));
+        data.rows = this.ranges.rows.min + Math.round(Math.random() * (this.ranges.rows.max - this.ranges.rows.min));
+
+        return data;
+    }
+
     createContainer(x) {
         this.container = new PIXI.Container();
         this.container.x = x;
